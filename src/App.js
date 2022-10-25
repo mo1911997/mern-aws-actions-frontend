995ce6d66adf81062a921a74560445fdc76ab108
mo1911997 <mmd26.aka@gmail.com>
2022-10-25 13:10:36 +0530

    message updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello Mohit </p>
+        <p>Hello Mohit 2</p>
       </header>
     </div>
   );
